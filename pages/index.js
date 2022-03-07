502297ccc7279a56ec578511798e97727bc7ef40
Asare Gideon <asaregid506@gmail.com>
2022-03-07 17:21:29 +0000

    home section

--- pages/index.js
+++ pages/index.js
@@ -2,134 +2,19 @@ import Head from 'next/head';
 import SideBar from '../src/components/SideBar';
 import Channelbar from '../src/components/ChannelBar';
 import ContentContainer from '../src/components/ContentContainer';
+import AreaChartComponent from '../src/components/AreaChart';
+import RadialChart from '../src/components/RadialChart';
 
 export default function Home() {
 	return (
 		<div className="home-container">
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
-			<h3>hello therr</h3>
+			<div>
+				<h1>Analytices</h1>
+			</div>
+			<div>
+				<AreaChartComponent />
+				<RadialChart />
+			</div>
 		</div>
 	);
 }
