d46a21909dbedd30657a985c76a8b6b9f005b776
Bboy <bboy@bboy.dev>
2022-03-08 10:15:59 -0500

    analytics update

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,99 @@
-import Head from 'next/head';
-import SideBar from '../src/components/SideBar';
-import Channelbar from '../src/components/ChannelBar';
-import ContentContainer from '../src/components/ContentContainer';
-import AreaChartComponent from '../src/components/AreaChart';
-import RadialChart from '../src/components/RadialChart';
+import Head from "next/head";
+import SideBar from "../src/components/SideBar";
+import Channelbar from "../src/components/ChannelBar";
+import ContentContainer from "../src/components/ContentContainer";
+import AreaChartComponent from "../src/components/AreaChart";
+import {
+  BsFillGiftFill,
+  BsFillHeartFill,
+  BsFillCreditCardFill,
+} from "react-icons/bs";
+import { BiBarChartAlt } from "react-icons/bi";
+import PieChartComponent from "../src/components/PieChart";
 
 export default function Home() {
-	return (
-		<div className="home-container">
-			<div>
-				<h1>Analytices</h1>
-			</div>
-			<div>
-				<AreaChartComponent />
-				<RadialChart />
-			</div>
-		</div>
-	);
+  return (
+    <div className="home-container">
+      <div className="ml-8 mt-3">
+        <h1 className="text-gray-600 font-semibold text-2xl dark:text-green-50">
+          Analytices
+        </h1>
+      </div>
+      <div className="flex pb-10 justify-evenly">
+        <div className=" bg-white dark:bg-slate-800 pt-3 pb-4 pl-2 pr-2  w-3/5 mt-2 shadow-lg rounded-sm p-2">
+          <div className="w-full flex justify-between">
+            <div>
+              <div className="mb-6">
+                <h3 className="text-gray-500 dark:text-gray-100 text-lg">
+                  Dashboard
+                </h3>
+                <p className="text-[10px] text-gray-500 dark:text-gray-400 font-semibold">
+                  Overview of Latest Month
+                </p>
+              </div>
+
+              <SideHeading name={"$3445.84"} text={"Current month earnings"} />
+              <SideHeading name={"89"} text={"Current month sales"} />
+
+              <div>
+                <h3 className="p-3 bg-green-600 text-white w-full rounded-md mb-2">
+                  Last Month Summary
+                </h3>
+              </div>
+            </div>
+            <AreaChartComponent />
+          </div>
+          <div className="mt-1 flex justify-between ">
+            <BottomItem
+              icon={<BsFillGiftFill size={"23"} color="green" />}
+              title={"Wallet Balance"}
+              text={"$3445.84"}
+            />
+            <BottomItem
+              icon={<BsFillHeartFill size={"25"} color="purple" />}
+              title={"Referral Earning"}
+              text={"$3445.84"}
+            />
+            <BottomItem
+              icon={<BsFillCreditCardFill size={"25"} color="#4285F4" />}
+              title={"Estimate sales"}
+              text={"$3445.84"}
+            />
+            <BottomItem
+              icon={<BiBarChartAlt size={"26"} color="#DB4437" />}
+              title={"Earning"}
+              text={"$3445.84"}
+            />
+          </div>
+        </div>
+
+        <div className=" bg-white dark:bg-slate-800 pt-3 pb-4 pl-3 pr-3  w-1/3 mt-3 shadow-lg rounded-sm p-2">
+          <PieChartComponent />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const SideHeading = ({ name, text }) => (
+  <div className="mb-6">
+    <h3 className="text-gray-700 dark:text-white text-xl font-bold">
+      {" "}
+      {name}{" "}
+    </h3>
+    <p className="text-[10px] dark:text-gray-400 text-gray-500 font-semibold">
+      {" "}
+      {text}{" "}
+    </p>
+  </div>
+);
+
+const BottomItem = ({ icon, title, text }) => (
+  <div className="mt-2 mr-1 flex items-center shadow-md w-1/4 pl-3 pt-2 pb-2 dark:bg-slate-900">
+    <div className="mr-2">{icon}</div>
+    <div>
+      <h3 className="text-gray-500 text-[15px] dark:text-zinc-50	"> {title} </h3>
+      <p className="text-gray-500 text-[12px] dark:text-zinc-300"> {text} </p>
+    </div>
+  </div>
+);
---
+++ src/components/PieChart/index.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { BsFacebook } from "react-icons/bs";
+
+function PieChartComponent() {
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  return (
+    <div>
+      <div>
+        <h3 className="text-gray-500 text-2xl font-semibold dark:text-zinc-50">
+          Trafic
+        </h3>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart width={100} height={200}>
+            <Pie
+              data={data}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex justify-around flex-wrap">
+        <div className=" shadow-md pl-5 pr-5 mt-3 dark:bg-slate-900">
+          <h2 className="text-xl text-gray-900 font-semibold dark:text-zinc-200">
+            40%
+          </h2>
+          <div className="flex items-center">
+            <div className="w-3 h-3 mr-1 bg-[#00C49F] rounded-full"></div>
+            <h4 className="text-[12px] text-gray-500 dark:text-zinc-200">
+              Youtube
+            </h4>
+          </div>
+        </div>
+        <div className=" shadow-md pl-5 pr-5 mt-3 dark:bg-slate-900">
+          <h2 className="text-xl text-gray-900 font-semibold dark:text-zinc-200">
+            10%
+          </h2>
+          <div className="flex items-center">
+            <div className="w-3 h-3 mr-1 bg-[#FF8042] rounded-full"></div>
+            <h4 className="text-[12px]  text-gray-500 dark:text-zinc-200">
+              Twiter
+            </h4>
+          </div>
+        </div>
+        <div className=" shadow-md pl-5 pr-5 mt-3 dark:bg-slate-900">
+          <h2 className="text-xl text-gray-900 font-semibold dark:text-zinc-200">
+            30%
+          </h2>
+          <div className="flex items-center">
+            <div className="w-3 h-3 mr-1 bg-blue-600 rounded-full"></div>
+            <h4 className="text-[12px]  text-gray-500 dark:text-zinc-200">
+              Facebook
+            </h4>
+          </div>
+        </div>
+        <div className="shadow-md pl-5 pr-5 ml-2 mt-3 dark:bg-slate-900">
+          <h2 className="text-xl text-gray-900 font-semibold dark:text-zinc-200">
+            20%
+          </h2>
+          <div className="flex items-center">
+            <div className="w-3 h-3 mr-1 bg-[#FFBB28] rounded-full"></div>
+            <h4 className="text-[12px]  text-gray-500 dark:text-zinc-200">
+              Dirrect search
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+  k;
+}
+
+export default PieChartComponent;
